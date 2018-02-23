50e0dff388d1048361f3cdaa769b5792802910dd
Philipp Erck <philipp.erck@gmx.de>
2018-02-23 14:40:04 +0100

    First commit

---
+++ script.js
@@ -0,0 +1,57 @@
+const slide = {
+  slideList: document.querySelector('.slider ul'),
+  slideListItem: document.querySelectorAll('.slider ul li'),
+  left: document.querySelector('.left'),
+  right: document.querySelector('.right'),
+  item: 0,
+  width: 720,
+  setActive() {
+    this.slideListItem[0].classList.add('active');
+  },
+  setDataItem() {
+    this.slideListItem.forEach((el) => {
+      el.setAttribute('data-item', this.item + 1);
+      this.item++;
+    });
+  },
+  getElementId() {
+    let itemClass = document.querySelector('.slider .active');
+    let itemId = itemClass.getAttribute('data-item');
+    return parseInt(itemId);
+  },
+  transition() {
+    let id = this.getElementId();
+    if (id != this.slideList.childElementCount) {
+      this.slideList.style.transform = 'translateX(-' + id * this.width + 'px)';
+      this.slideList.classList.add('slide');
+    } else {
+      this.slideList.style.transform = 'translateX(0px)';
+      this.slideList.classList.add('slide');
+    }
+  },
+  getActive() {
+    let itemClass = document.querySelector('.slider .active');
+    itemClass.classList.remove('active');
+    if (itemClass.nextSibling.nextSibling != null) {
+      let sibling = itemClass.nextSibling.nextSibling;
+      sibling.classList.add('active');
+    } else {
+      this.slideListItem[0].classList.add('active');
+    }
+  },
+  start() {
+    this.setActive();
+    this.setDataItem();
+  },
+  click() {
+    this.transition();
+    this.getActive();
+  }
+};
+
+slide.start();
+
+slide.right.addEventListener('click', () => slide.click());
+
+
+
