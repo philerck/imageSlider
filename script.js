44695f840c3b3eaa33eda2cc082aeaaa39e6a56e
Philipp Erck <philipp.erck@gmx.de>
2018-02-25 18:35:05 +0100

    Slider links - slider -rechts working

--- script.js
+++ script.js
@@ -1,63 +1,25 @@
 const slide = {
   slideList: document.querySelector('.slider ul'),
   slideListItem: document.querySelectorAll('.slider ul li'),
-  left: document.querySelector('.left'),
-  right: document.querySelector('.right'),
-  item: 0,
   width: document.querySelector('.slider ul'),
   transform: 0,
-  setActive() {
-    this.slideListItem[0].classList.add('active');
+
+  transition(el) {
+
+    el.target.classList.contains('left') ? this.transform += this.width.offsetWidth : this.transform -= this.width.offsetWidth;
+
+    this.slideList.style.transform = 'translateX(' + this.transform + 'px)';
+    this.slideList.classList.add('slide');
   },
-  setDataItem() {
-    this.slideListItem.forEach((el) => {
-      el.setAttribute('data-item', this.item + 1);
-      this.item++;
-    });
-  },
-  getElementId() {
-    let itemClass = document.querySelector('.slider .active');
-    let itemId = itemClass.getAttribute('data-item');
-    return parseInt(itemId);
-  },
-  transition() {
-    let id = this.getElementId();
-    console.log(this.width.offsetWidth);
-    if (id != this.slideList.childElementCount) {
-      this.slideList.style.transform = 'translateX(-' + this.transform + this.width.offsetWidth + 'px)';
-      this.slideList.classList.add('slide');
-    } else {
-      this.slideList.style.transform = 'translateX(0px)';
-      this.slideList.classList.add('slide');
-    }
-  },
-  getActive() {
-    let itemClass = document.querySelector('.slider .active');
-    itemClass.classList.remove('active');
-    if (itemClass.nextSibling.nextSibling != null) {
-      let sibling = itemClass.nextSibling.nextSibling;
-      sibling.classList.add('active');
-    } else {
-      this.slideListItem[0].classList.add('active');
-    }
-  },
-  start() {
-    this.setActive();
-    this.setDataItem();
-  },
-  click() {
-    //console.log(el);
-    //let symbol;
-    //el.target.classList.contains('left') ? symbol = '' : symbol = '-';
-    this.transition();
-    this.getActive();
+  click(el) {
+
+    this.transition(el);
+    //this.getActive();
   }
 };
-
-slide.start();
-
-slide.right.addEventListener('click', () => slide.click());
-slide.left.addEventListener('click', () => slide.click());
+slide.arrow.forEach(function (el) {
+  el.addEventListener('click', (el) => slide.click(el));
+}););
 
 
 
