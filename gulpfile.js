1fae1e33bda4c0076f52fde9bcd1ad70810aa369
Philipp Erck <philipp.erck@gmx.de>
2018-02-27 13:23:32 +0100

    Add round Buttons

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,11 @@ let gulp = require('gulp');
 let sass = require('gulp-sass');
 let cleanCSS = require('gulp-clean-css');
 let rename = require('gulp-rename');
+let webserver = require('gulp-webserver');
+
+gulp.task('serve', ['webserver'], function () {
+  gulp.watch('./style.scss', ['css']);
+});
 
 gulp.task('css', function () {
   return gulp.src('*.scss')
@@ -9,4 +14,13 @@ gulp.task('css', function () {
     .pipe(cleanCSS())
     .pipe(rename({ suffix: '.min' }))
     .pipe(gulp.dest('./'));
+});
+
+gulp.task('webserver', function () {
+  gulp.src('./')
+    .pipe(webserver({
+      livereload: true,
+      port: 8080,
+      host: '0.0.0.0'
+    }));
 });
--- script.js
+++ script.js
@@ -3,12 +3,12 @@ const slide = {
   slideListItem: document.querySelectorAll('.slider ul li'),
   width: document.querySelector('.slider ul'),
   arrow: document.querySelectorAll('.arrow'),
-  maxWidth: 720,
+  maxWidth: 650,
   transform: 0,
   transition(el) {
     let move = 100;
 
-    el.target.classList.contains('left') ? this.transform += move : this.transform -= move;
+    el.target.classList.contains('left') || el.target.parentElement.classList.contains('left') ? this.transform += move : this.transform -= move;
 
     if (this.transform > 0 || this.transform === ((this.slideList.childElementCount * 100)) * -1) {
       this.transform = 0;
@@ -17,15 +17,12 @@ const slide = {
     //this.slideList.classList.add('slide');
 
   },
-  click(el) {
-    this.transition(el);
-  },
   start() {
-    let wrapper = document.querySelector('.slider__wrapper');
+    let wrapper = document.querySelector('.slider');
     wrapper.style.maxWidth = this.maxWidth + 'px';
     this.slideList.style.transition = 'transform 1s ease-in';
     slide.arrow.forEach(function (el) {
-      el.addEventListener('click', (el) => slide.click(el));
+      el.addEventListener('click', (el) => slide.transition(el));
     });
   }
 };
