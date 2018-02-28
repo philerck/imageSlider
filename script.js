5556a84b8c96491925987b9a2e7606f1af48bda9
Philipp Erck <philipp.erck@gmx.de>
2018-02-28 12:26:24 +0100

    add Bullets

--- script.js
+++ script.js
@@ -3,8 +3,9 @@ const slide = {
   slideListItem: document.querySelectorAll('.slider ul li'),
   width: document.querySelector('.slider ul'),
   arrow: document.querySelectorAll('.arrow'),
-  maxWidth: 650,
+  maxWidth: 720,
   transform: 0,
+  countImages: document.querySelector('.slider ul').childElementCount,
   transition(el) {
     let move = 100;
 
@@ -17,13 +18,31 @@ const slide = {
     //this.slideList.classList.add('slide');
 
   },
+  allElementsGetItemClass() {
+    let itemNumber = 0;
+    this.slideListItem.forEach(el => {
+      el.id = 'item_' + itemNumber;
+      itemNumber += 1;
+    });
+  },
   start() {
+    this.allElementsGetItemClass();
+
+    let firstLiElement = this.slideList.querySelector('li:first-child');
+    firstLiElement.classList.add('active');
+
     let wrapper = document.querySelector('.slider');
     wrapper.style.maxWidth = this.maxWidth + 'px';
+
     this.slideList.style.transition = 'transform 1s ease-in';
+
     slide.arrow.forEach(function (el) {
       el.addEventListener('click', (el) => slide.transition(el));
     });
+
+    let active = document.querySelector('.slider ul li.active');
+    let activeIndex = active.getAttribute('id');
+    console.log(activeIndex.split('_'));
   }
 };
 
