6401e7d429a282e06126036a809d84d87d8342c6
Philipp Erck <philipp.erck@gmx.de>
2018-03-01 16:16:10 +0100

    Test for simler Slide

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 const slide = {
   slideList: document.querySelector('.slider ul'),
   slideListItem: document.querySelectorAll('.slider ul li'),
+  bullets: document.querySelectorAll('.bulletsForImages__bullet'),
   width: document.querySelector('.slider ul'),
-  arrow: document.querySelectorAll('.arrow'),
   maxWidth: 650,
   transform: 0,
   transition(el) {
@@ -17,13 +17,55 @@ const slide = {
     //this.slideList.classList.add('slide');
 
   },
+  setNewElementActive(index, direction) {
+    console.log(direction);
+    if (direction < 0) {
+      this.slideListItem[index - 1].classList.remove('active');
+      this.slideListItem[index].classList.add('active');
+    } else {
+      this.slideListItem[index - 1].classList.remove('active');
+      this.slideListItem[index - 2].classList.add('active');
+    }
+  },
+  move(direction) {
+    let index = this.getActiveID();
+    this.setNewElementActive(index, direction);
+    let transformInProzent = index * direction;
+    this.slideList.style.transform = 'translateX(' + transformInProzent + '%)';
+  },
+  getActiveID() {
+    let activeElement = document.querySelector('.slider ul li.active');
+    activeElement = activeElement.getAttribute('id').split('_');
+    let activeElementIndex = activeElement[1];
+    return activeElementIndex;
+  },
+  allElementsGetItemClass() {
+    let itemNumber = 1;
+    this.slideListItem.forEach(el => {
+      el.id = 'item_' + itemNumber;
+      itemNumber += 1;
+    });
+    let bulletNumber = 1;
+    this.bullets.forEach(el => {
+      el.id = 'bullet_' + bulletNumber;
+      bulletNumber += 1;
+    });
+  },
   start() {
+    this.allElementsGetItemClass();
+
+    let firstLiElement = this.slideList.querySelector('li:first-child');
+    firstLiElement.classList.add('active');
+
     let wrapper = document.querySelector('.slider');
     wrapper.style.maxWidth = this.maxWidth + 'px';
     this.slideList.style.transition = 'transform 1s ease-in';
-    slide.arrow.forEach(function (el) {
-      el.addEventListener('click', (el) => slide.transition(el));
-    });
+
+    let leftBtn = document.querySelector('img.left');
+    leftBtn.addEventListener('click', () => this.move(100));
+
+    let rightBtn = document.querySelector('img.right');
+    rightBtn.addEventListener('click', () => this.move(-100));
   }
 };
 
